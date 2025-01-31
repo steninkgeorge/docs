3820679e5f48f0836cfd6670fc20026631d5e6b0
Stenin K George <steninkgeorge007@gmail.com>
2025-01-31 21:23:00 +0530

    link

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -9,9 +9,17 @@ import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
 import Image from "@tiptap/extension-image";
+import TextStyle from "@tiptap/extension-text-style";
 import ImageResize from "tiptap-extension-resize-image";
 import Underline from "@tiptap/extension-underline";
+import FontFamily from "@tiptap/extension-font-family";
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Color } from "@tiptap/extension-color";
+import Link from "@tiptap/extension-link";
+
+
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -60,31 +68,100 @@ export const Editor = () => {
       StarterKit,
       Underline,
       TaskList,
+      TextStyle,
+      Link.configure({
+        HTMLAttributes:{
+          class:'text-blue-500 underline cursor-pointer'
+        },
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+        protocols: ["http", "https"],
+        isAllowedUri: (url, ctx) => {
+          try {
+            // construct URL
+            const parsedUrl = url.includes(":")
+              ? new URL(url)
+              : new URL(`${ctx.defaultProtocol}://${url}`);
+
+            // use default validation
+            if (!ctx.defaultValidate(parsedUrl.href)) {
+              return false;
+            }
+
+            // disallowed protocols
+            const disallowedProtocols = ["ftp", "file", "mailto"];
+            const protocol = parsedUrl.protocol.replace(":", "");
+
+            if (disallowedProtocols.includes(protocol)) {
+              return false;
+            }
+
+            // only allow protocols specified in ctx.protocols
+            const allowedProtocols = ctx.protocols.map((p) =>
+              typeof p === "string" ? p : p.scheme
+            );
+
+            if (!allowedProtocols.includes(protocol)) {
+              return false;
+            }
+
+            // disallowed domains
+            const disallowedDomains = [
+              "example-phishing.com",
+              "malicious-site.net",
+            ];
+            const domain = parsedUrl.hostname;
+
+            if (disallowedDomains.includes(domain)) {
+              return false;
+            }
+
+            // all checks have passed
+            return true;
+          } catch {
+            return false;
+          }
+        },
+        shouldAutoLink: (url) => {
+          try {
+            // construct URL
+            const parsedUrl = url.includes(":")
+              ? new URL(url)
+              : new URL(`https://${url}`);
+
+            // only auto-link if the domain is not in the disallowed list
+            const disallowedDomains = [
+              "example-no-autolink.com",
+              "another-no-autolink.com",
+            ];
+            const domain = parsedUrl.hostname;
+
+            return !disallowedDomains.includes(domain);
+          } catch {
+            return false;
+          }
+        },
+      })
+      ,
       TaskItem.configure({
         nested: true,
       }),
       Table.configure({
         resizable: true,
       }),
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+      }),
+      Highlight.configure({ multicolor: true }),
       TableRow,
       TableHeader,
       TableCell,
       ImageResize,
+      FontFamily,
+      Color,
     ],
-    content: `        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
+    content: `        
 `,
   });
   return (
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,149 +1,360 @@
-'use client'
+"use client";
 
-import { cn } from "@/lib/utils"
-import { useEditorStore } from "@/store/use-editor-store"
-import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquareCodeIcon, MessageSquareDashed, MessageSquareIcon, MessageSquarePlus, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormatting, RemoveFormattingIcon, SpellCheck, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react"
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/use-editor-store";
+import {
+  BoldIcon,
+  ChevronDownIcon,
+  HighlighterIcon,
+  Icon,
+  ItalicIcon,
+  Link2Icon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquareCodeIcon,
+  MessageSquareDashed,
+  MessageSquareIcon,
+  MessageSquarePlus,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormatting,
+  RemoveFormattingIcon,
+  SpellCheck,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading";
+import { useCallback, useState } from "react";
+import Highlight from "@tiptap/extension-highlight";
+import { CirclePicker, SketchPicker, type ColorResult } from "react-color";
+import { useEditor } from "@tiptap/react";
 
-interface ToolBarIconProps{
-    onClick?:()=> void,
-    isActive?:boolean,
-    icon: LucideIcon
+interface ToolBarIconProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
 }
 
+type ToolbarItemType = {
+  label: string;
+  icon: LucideIcon;
+  onClick: () => void;
+  isActive?: boolean;
+}[][];
 
-const ToolBarButton=({
-    onClick, 
-    isActive,
-    icon: Icon
-}: ToolBarIconProps)=>{
-    return (
-      <button
-        onClick={onClick}
-        className={cn(
-          "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm bg-neutral-200/80 hover:bg-neutral-500/80",
-          isActive && "bg-neutral-500/80"
-        )}
-      >
-        <Icon className="size-4" />
-      </button>
-    );
-}
+const ToolBarButton = ({ onClick, isActive, icon: Icon }: ToolBarIconProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm bg-neutral-200/80 hover:bg-neutral-500/80",
+        isActive && "bg-neutral-500/80"
+      )}
+    >
+      <Icon className="size-4" />
+    </button>
+  );
+};
 
-export const Toolbar=()=>{
-    const {editor}=useEditorStore()
+const TextColorButton = () => {
+  const { editor } = useEditorStore();
 
-    const sections: {
-      label: string;
-      icon: LucideIcon;
-      onClick: () => void;
-      isActive?: boolean;
-    }[][] = [
-      [
-        {
-          label: "undo",
-          icon: Undo2Icon,
-          onClick: () => {
-            editor?.chain().focus().undo().run();
-          },
-          isActive: editor?.isActive("undo"),
+  const onChange = (value: ColorResult) => {
+    editor?.chain().focus().setColor(value.hex).run();
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="flex flex-col h-7 min-w-7 p-2 rounded-sm hover:bg-neutral-200/80 items-center justify-center">
+          A
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <SketchPicker onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingButton = () => {
+  const { editor } = useEditorStore();
+
+  const heading = [
+    { label: "Normal Text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
+
+  const getButtonLabel = () => {
+    for (let i = 1; i <= 5; i++) {
+      if (editor?.isActive("heading", { level: i })) {
+        return `heading ${i}`;
+      }
+    }
+    return "Normal Text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 text-sm flex items-center p-2 rounded-sm hover:bg-neutral-200/80">
+          <span className="truncate">{getButtonLabel()}</span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {heading.map(({ label, value, fontSize }) => (
+          <button
+            key={label}
+            className="flex items-center hover:bg-neutral-200/80 w-full justify-center"
+            style={{ fontSize: fontSize }}
+            onClick={() => {
+              console.log(value);
+              value === 0
+                ? editor?.commands.setParagraph()
+                : editor
+                    ?.chain()
+                    .focus()
+                    .setHeading({ level: value as Level })
+                    .run();
+            }}
+          >
+            <span>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const FontFamilyButton = () => {
+  const [open, setOpen] = useState(false);
+  const handleSelect = () => {
+    setOpen(false);
+  };
+
+  const { editor } = useEditorStore();
+
+  const fonts = [
+    { label: "Arial", value: "Arial" },
+    { label: "Times New Roman", value: "Times New Roman" },
+    { label: "Courier New", value: "Courier New" },
+    { label: "Georgia", value: "Georgia" },
+    { label: "Verdana", value: "Verdana" },
+  ];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 text-sm w-[120px] hover:bg-neutral-200/80 rounded-sm shrink-0 justify-between flex items-center px-2 outline-none">
+          <span className="truncate">
+            {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {fonts.map(({ label, value }) => (
+          <button
+            key={value}
+            className={cn(
+              "flex item-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80 w-full",
+              editor?.isActive("textStyle", { fontFamily: value }) &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontFamily: value }}
+            onClick={() => {
+              editor?.chain().focus().setFontFamily(value).run();
+            }}
+          >
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export const Toolbar = () => {
+  const { editor } = useEditorStore();
+
+  const sections: ToolbarItemType = [
+    [
+      {
+        label: "undo",
+        icon: Undo2Icon,
+        onClick: () => {
+          editor?.chain().focus().undo().run();
         },
-        {
-          label: "redo",
-          icon: Redo2Icon,
-          onClick: () => {
-            editor?.chain().focus().redo().run();
-          },
+        isActive: editor?.isActive("undo"),
+      },
+      {
+        label: "redo",
+        icon: Redo2Icon,
+        onClick: () => {
+          editor?.chain().focus().redo().run();
         },
-        {
-          label: "print",
-          icon: PrinterIcon,
-          onClick: () => {
-            window.print();
-          },
+      },
+      {
+        label: "print",
+        icon: PrinterIcon,
+        onClick: () => {
+          window.print();
         },
-        {
-          label: "spellcheck",
-          icon: SpellCheckIcon,
-          onClick: () => {
-            const current = editor?.view.dom.getAttribute("spellcheck");
-            editor?.view.dom.setAttribute(
-              "spellcheck",
-              current === "false" ? "true" : "false"
-            );
-          },
+      },
+      {
+        label: "spellcheck",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
         },
-      ],
-      [
-        {
-          label: "bold",
-          icon: BoldIcon,
-          onClick: () => {
-            const active = editor?.isActive("bold");
-            active
-              ? editor?.chain().focus().unsetBold().run()
-              : editor?.chain().focus().setBold().run();
-          },
-          isActive: editor?.isActive("bold"),
+      },
+    ],
+    [
+      {
+        label: "bold",
+        icon: BoldIcon,
+        onClick: () => {
+          const active = editor?.isActive("bold");
+          active
+            ? editor?.chain().focus().unsetBold().run()
+            : editor?.chain().focus().setBold().run();
         },
-        {
-          label: "italic",
-          icon: ItalicIcon,
-          onClick: () => {
-            const active = editor?.isActive("italic");
-            active
-              ? editor?.chain().focus().unsetItalic().run()
-              : editor?.chain().focus().setItalic().run();
-          },
-          isActive: editor?.isActive("italic"),
+        isActive: editor?.isActive("bold"),
+      },
+      {
+        label: "italic",
+        icon: ItalicIcon,
+        onClick: () => {
+          const active = editor?.isActive("italic");
+          active
+            ? editor?.chain().focus().unsetItalic().run()
+            : editor?.chain().focus().setItalic().run();
         },
-        {
-          label: "underline",
-          icon: UnderlineIcon,
-          onClick: () => {
-            const active = editor?.isActive("underline");
-            active
-              ? editor?.chain().focus().unsetUnderline().run()
-              : editor?.chain().focus().toggleUnderline().run();
-          },
-          isActive: editor?.isActive("underline"),
+        isActive: editor?.isActive("italic"),
+      },
+      {
+        label: "underline",
+        icon: UnderlineIcon,
+        onClick: () => {
+          const active = editor?.isActive("underline");
+          active
+            ? editor?.chain().focus().unsetUnderline().run()
+            : editor?.chain().focus().toggleUnderline().run();
         },
-      ],
-      [
-        {
-          label: "comment",
-          icon: MessageSquarePlusIcon,
-          onClick: () => {
-            console.log("TODO: comment");
-          },
-          isActive: editor?.isActive("comment"),
+        isActive: editor?.isActive("underline"),
+      },
+    ],
+    [
+      {
+        label: "comment",
+        icon: MessageSquarePlusIcon,
+        onClick: () => {
+          console.log("TODO: comment");
         },
-        {
-          label: "List Todo",
-          icon: ListTodoIcon,
-          onClick: () => {
-            editor?.chain().focus().toggleTaskList().run();
-          },
-          isActive: editor?.isActive("tasklist"),
+        isActive: editor?.isActive("comment"),
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => {
+          editor?.chain().focus().toggleTaskList().run();
         },
-        {
-          label: "Remove Formatting",
-          icon: RemoveFormattingIcon,
-          onClick: () => {
-            editor?.chain().focus().unsetAllMarks().run();
-          },
+        isActive: editor?.isActive("tasklist"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => {
+          editor?.chain().focus().unsetAllMarks().run();
         },
-      ],
-    ];
-    return (
-        <div className="px-2.5 py-0.5 bg-[#F1F4F9] rounded-[24px] min-h-[40px] overflow-x-auto flex items-center">
-           {
-            sections.map((row,rowIndex)=>(
-                <div key={rowIndex} className="flex mx-2 gap-x-0.5">{
-                   row.map((item)=>(
-                    <ToolBarButton key={item.label} icon={item.icon} onClick={item.onClick} isActive={item.isActive} />
-                   )) } </div>
-            ))
-           }
+      },
+      {
+        label: "highlight",
+        icon: HighlighterIcon,
+        onClick: () => {
+          editor?.chain().focus().toggleHighlight().run();
+        },
+        isActive: editor?.isActive("highlight"),
+      },
+      {
+        label: "link",
+        icon: Link2Icon,
+        onClick: editor?.isActive("link")
+          ? () => {
+            editor.commands.unsetMark("link"); // Ensure next text isn't linked
+          }
+          : () => {
+              if (!editor) {
+                return;
+              }
+              const previousUrl = editor.getAttributes("link").href;
+              const url = window.prompt("URL", previousUrl);
+
+              if (url === null) {
+                return;
+              }
+
+              // empty
+              if (url === "") {
+                editor
+                  .chain()
+                  .focus()
+                  .extendMarkRange("link")
+                  .unsetLink()
+                  .run();
+
+                return;
+              }
+              editor
+                .chain()
+                .focus()
+                .extendMarkRange("link")
+                .setLink({ href: url })
+                .run();
+            },
+        isActive: editor?.isActive("link"),
+      },
+    ],
+  ];
+  return (
+    <div className="px-2.5 py-0.5 bg-[#F1F4F9] rounded-[24px] min-h-[40px] overflow-x-auto flex items-center">
+      {sections.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex mx-2 gap-x-0.5">
+          {row.map((item) => (
+            <ToolBarButton
+              key={item.label}
+              icon={item.icon}
+              onClick={item.onClick}
+              isActive={item.isActive}
+            />
+          ))}
+          <Separator
+            orientation="vertical"
+            className="mx-2 h-6 bg-neutral-300"
+          />
         </div>
-    )
-}
+      ))}
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="mx-2 h-6 bg-neutral-300" />
+      <HeadingButton />
+      <TextColorButton />
+    </div>
+  );
+};
